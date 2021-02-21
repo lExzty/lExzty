397c7e613aa1af245dc4a05d87bcd2a6e42653dc
lExzty <79412547+lExzty@users.noreply.github.com>
2021-02-21 17:33:13 +0100

    Add files via upload

---
+++ title.js
@@ -0,0 +1,3 @@
+if(document.addEventListener){document.addEventListener("DOMContentLoaded",function(){loaded();});}else if(document.attachEvent){document.attachEvent("onreadystatechange",function(){loaded();});}
+function loaded(){setInterval(loop,400);}
+var x=0;var titleText=["lExzty.tools","lExzty.tool","lExzty.too","lExzty.to","lExzty.t","lExzty.","lExzty","lExzt","lExz","lEx","lE"];function loop(){document.getElementsByTagName("lExzty")[0].innerHTML=titleText[x++%titleText.length];}
